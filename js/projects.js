a85875ab8ff6e6bc6fc1b119edfc39e1af9a1216
kfmahre <kfmahre@gmail.com>
2016-06-10 16:54:31 -0400

    Figured out how to get jekyll and angular to work together with $interpolateProvider!

---
+++ js/projects.js
@@ -0,0 +1,51 @@
+  var projects = [
+    {
+      name: 'Responsive Portfolio',
+      projectUrl: "http://kfmahre.github.io/Portfolio/",
+      projectImg: "http://placehold.it/350x150",
+      gitUrl: "https://github.com/kfmahre/Portfolio/",
+      description:"My simple, Responsive Portfolio"
+    },
+    {
+      name: "Interactive Resume",
+      projectUrl: "http://kfmahre.github.io/Resume/",
+      projectImg: "http://placehold.it/350x150",
+      gitUrl: "https://github.com/kfmahre/Resume/",
+      description:"Demonstrates jQuery and JSON mastery<br>Features a CSS3 parallax background effect & Slick project carousel <br> Technologies used: jQuery, HTML & CSS, JavaScript"
+    },
+   {
+      name: "Classic Arcade Game Clone",
+      projectUrl: "https://github.com/kfmahre/arcade-game/",
+      projectImg: "http://placehold.it/350x150",
+      gitUrl: "https://github.com/kfmahre/arcade-game/",
+      description:"Frogger style game clone <br> Technologies used: JavaScript, HTML5 Canvas"
+    },
+    {
+      name: "Website Performance Optimization",
+      projectUrl: "https://github.com/kfmahre/udportfolio/",
+      projectImg: "http://placehold.it/350x150",
+      gitUrl: "https://github.com/kfmahre/udportfolio/",
+      description:"Improved PageSpeed score of an existing website from x to above 90 on both mobile and desktop. (Steps documented on github) <br>Optimized JavaScript for a smooth 60fps on scroll. <br>Technologies used: Gulp, Ngrok, and Chrome developer tools"
+    },
+    {
+      name: "Cat-Clicker Web-App",
+      projectUrl: "http://kfmahre.github.io/Cat-Clicker/",
+      projectImg: "http://placehold.it/350x150",
+      gitUrl: "https://github.com/kfmahre/Cat-Clicker/",
+      description:"This was a fun little project I used to learn how to seperate the model and view of a simple web-app. It turned into something not-so-simple."
+    },
+    {
+      name: "Neighborhood-Map",
+      projectUrl: "http://kfmahre.github.io/Neighborhood-Map/",
+      projectImg: "http://placehold.it/350x150",
+      gitUrl: "https://github.com/kfmahre/Neighborhood-Map/",
+      description:"Search field filters a list-view & Google map markers. <br> Wikipedia & wUnderground Ajax <br> Technologies used: Knockout.js, AJAX, Bootstrap, JavaScript, HTML & CSS"
+    },
+    {
+      name: "Angular.js Gem-Store",
+      projectUrl: "http://kfmahre.github.io/angular_store/",
+      projectImg: "http://placehold.it/350x150",
+      gitUrl: "https://github.com/kfmahre/angular_store/",
+      description:"A store App, Made with angular.js"
+    }
+  ];
